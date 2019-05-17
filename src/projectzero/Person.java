63e16c15aaa8c9624f79b50fbd6ed0611a62c267
Németh Mátyás <matyi714@gmail.com>
2019-05-17 13:34:08 +0200

    Person class initialized

---
+++ src/projectzero/Person.java
@@ -0,0 +1,31 @@
+
+package projectzero;
+
+
+public class Person {
+    
+    private String name;
+    private String city;
+    private String email;
+
+    public Person(String name, String city, String email) {
+        this.name = name;
+        this.city = city;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    
+
+}
