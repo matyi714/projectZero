76529642cc5783c0da4e923ad2e1c41233d9f402
Varga János <vargajana95@gmail.com>
2019-05-17 14:09:45 +0200

    List persons from given city

--- src/projectzero/ProjectZero.java
+++ src/projectzero/ProjectZero.java
@@ -4,6 +4,7 @@ package projectzero;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class ProjectZero {
@@ -16,6 +17,26 @@ public class ProjectZero {
         List<Person> ch = Filter.cityFilter("Chicago", al);
         for (Person person : ch) {
             System.out.println(person.getEmail());
+        }
+    }
+    
+    public static void printPersonsFromCity() throws FileNotFoundException {
+        System.out.println("Kérem adjon meg egy várost:");
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String city = sc.nextLine();
+        
+        List<Person> persons = Filter.cityFilter(city, Reader.read());
+        
+        if (persons.isEmpty()) {
+            System.out.println("Ebben a városban lakó emberről nincs adatunk!");
+        }
+        else {
+            System.out.println("Az ebben a városban lakó emberek:");
+            for (Person person : persons) {
+                System.out.println(person.getName());
+            }
         }
     }
 
