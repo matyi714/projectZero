f7057741065cf693b5ce43698d73ded673db0e45
Németh Mátyás <matyi714@gmail.com>
2019-05-17 14:10:30 +0200

    Refactor Main

--- src/projectzero/ProjectZero.java
+++ src/projectzero/ProjectZero.java
@@ -1,19 +1,22 @@
-
 package projectzero;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class ProjectZero {
 
-    
+    public static List<Person> al = new ArrayList<>();
+    public static List<Person> ch = new ArrayList<>();
+
     public static void main(String[] args) throws FileNotFoundException {
-        List<Person> al = new ArrayList<>();
         al = Reader.read();
         al.sort(new nameCoparator());
-        List<Person> ch = Filter.cityFilter("Chicago", al);
+        ch = Filter.cityFilter("Chicago", al);
+        chicagoPeople();
+    }
+
+    public static void chicagoPeople() {
         for (Person person : ch) {
             System.out.println(person.getEmail());
         }
